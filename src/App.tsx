3e28da4091b3f1a1b00970a22ecdf2572752ea47
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-05 13:50:31 +0200

    header styles added 05.05.23 13.50

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,16 @@ import './App.css';
 export const App = () => {
   return (
     <>
-      <header></header>
+      <header>
+        <h1>
+          <strong>CRAG </strong> finder
+        </h1>
+        <button>Add crag to climbing community</button>
+        <div className="search">
+          <input type="text" />
+          <button>Search for crags</button>
+        </div>
+      </header>
       <div className="map">************</div>
     </>
   );
