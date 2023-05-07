199ee196ce3f91e230a9914cb453720ca8029582
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-07 11:35:28 +0200

    basic search feature added 07.05.23 11.35

--- src/components/layout/Header/Header.tsx
+++ src/components/layout/Header/Header.tsx
@@ -1,16 +1,36 @@
-import React from 'react';
+import React, {
+  FormEvent,
+  FormEventHandler,
+  useContext,
+  useState,
+} from 'react';
 import './Header.css';
 import { Btn } from '../common/index';
+import { SearchContext } from '../../../context/search.context';
 
-export const Header = () => (
-  <header>
-    <h1>
-      <strong>CRAG </strong> finder
-    </h1>
-    <Btn text="Add crag to climbing community"></Btn>
-    <div className="search">
-      <input type="text" />
-      <Btn text="Search for crags" />
-    </div>
-  </header>
-);
+export const Header = () => {
+  const { search, setSearch } = useContext(SearchContext);
+  const [inputValue, setInputValue] = useState(search);
+
+  const setStateFromLocalState = (e: FormEvent) => {
+    e.preventDefault();
+    setSearch(inputValue);
+  };
+
+  return (
+    <header>
+      <h1>
+        <strong>CRAG </strong> finder
+      </h1>
+      <Btn text="Add crag to climbing community"></Btn>
+      <form className="search" onSubmit={setStateFromLocalState}>
+        <input
+          type="text"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        <Btn text="Search for crags" />
+      </form>
+    </header>
+  );
+};
--- src/components/layout/Map/Map.tsx
+++ src/components/layout/Map/Map.tsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { MapContainer, Marker, Popup, TileLayer } from 'react-leaflet';
+import { SearchContext } from '../../../context/search.context';
 import '../../../utils/fix-map-icon';
 import './Map.css';
 import 'leaflet/dist/leaflet.css';
 
 export const Map = () => {
+  const { search } = useContext(SearchContext);
+
   return (
     <div className="map">
+      <h1>Search for: {search}</h1>
       <MapContainer center={[50.6311796, 15.260342]} zoom={13}>
         <TileLayer
           url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
