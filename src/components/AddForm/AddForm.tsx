c6ca6c6fdc09f5f6291cb9dbb4ae9cedf7ac56d9
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-09 19:22:24 +0200

    form styling in progress 09.05.23 19:22

--- src/components/AddForm/AddForm.tsx
+++ src/components/AddForm/AddForm.tsx
@@ -1,10 +1,18 @@
 import { format } from 'path';
 import React, { useState } from 'react';
 
+import './AddForm.css';
+
 export const AddForm = () => {
   return (
-    <form>
+    <form className="add-form">
       <h1>Adding new crag</h1>
+      <p>
+        <label>
+          Name: <br />
+          <input type="text" name="name" required maxLength={99} />
+        </label>
+      </p>
     </form>
   );
 };
