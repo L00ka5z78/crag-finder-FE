7ecce5470b14781415096f0c8b0bdfedfb658175
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-09 18:47:26 +0200

    dependencies added 09.05.23 18:47

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 import { Header } from './components/layout/Header/Header';
 import { Map } from './components/layout/Map/Map';
 import { SearchContext } from './context/search.context';
+import { Route, Routes } from 'react-router-dom';
 
 export const App = () => {
   const [search, setSearch] = useState('');
@@ -10,7 +11,9 @@ export const App = () => {
       <SearchContext.Provider value={{ search, setSearch }}>
         <div className="wrapper">
           <Header />
-          <Map />
+          <Routes>
+            <Route path="/" element={<Map />} />
+          </Routes>
         </div>
       </SearchContext.Provider>
     </>
