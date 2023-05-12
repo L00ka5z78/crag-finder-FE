08a9d4ec9a03a26d0b1672a863a9681941fdf329
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-12 17:59:49 +0200

    clientApi reqest  & getUrl method 12.05.23 17:59

--- src/utils/clientApi/clientApi.ts
+++ src/utils/clientApi/clientApi.ts
@@ -2,4 +2,28 @@ type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE';
 
 export class ClientApi {
   constructor(private readonly baseUrl: string) {}
+
+  private getUrl(endpoint: string): string {
+    return this.baseUrl + endpoint;
+  }
+
+  private async request<RequestBodyT>(
+    endpoint: string,
+    method: HttpMethod,
+    options: RequestInit = {},
+    body?: RequestBodyT
+  ): Promise<Response> {
+    const fetchOptions: RequestInit = {
+      method,
+      ...options,
+    };
+    if (body) {
+      fetchOptions.headers = {
+        'Content-Type': 'application/json',
+        ...options.headers,
+      };
+      fetchOptions.body = JSON.stringify(body);
+    }
+    return await fetch(this.getUrl(endpoint), fetchOptions);
+  }
 }
