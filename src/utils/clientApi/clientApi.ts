b8515eaa7ecc7febafdffc80ef7600bb08754b94
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-05-12 18:08:56 +0200

    clientApi CRUD added 12.05.23 18:08

--- src/utils/clientApi/clientApi.ts
+++ src/utils/clientApi/clientApi.ts
@@ -1,4 +1,4 @@
-type HttpMethod = 'GET' | 'POST' | 'PUT' | 'DELETE';
+type HttpMethod = 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE';
 
 export class ClientApi {
   constructor(private readonly baseUrl: string) {}
@@ -25,5 +25,37 @@ export class ClientApi {
       fetchOptions.body = JSON.stringify(body);
     }
     return await fetch(this.getUrl(endpoint), fetchOptions);
+  }
+
+  async post<RequestBody>(
+    endpoint: string,
+    body: RequestBody,
+    options?: RequestInit
+  ): Promise<Response> {
+    return this.request(endpoint, 'POST', options, body);
+  }
+
+  async get(endpoint: string, options?: RequestInit): Promise<Response> {
+    return this.request(endpoint, 'GET', options);
+  }
+
+  async put<RequestBody>(
+    endpoint: string,
+    body: RequestBody,
+    options?: RequestInit
+  ): Promise<Response> {
+    return this.request(endpoint, 'PUT', options, body);
+  }
+
+  async patch<RequestBody>(
+    endpoint: string,
+    body: RequestBody,
+    options?: RequestInit
+  ): Promise<Response> {
+    return this.request(endpoint, 'PATCH', options, body);
+  }
+
+  async delete(endpoint: string, options?: RequestInit): Promise<Response> {
+    return this.request(endpoint, 'DELETE', options);
   }
 }
