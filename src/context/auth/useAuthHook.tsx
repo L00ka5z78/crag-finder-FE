85513e589658ca330e8b5754aee76bb6407e1c16
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-06-01 08:38:32 +0200

    custom authHok added, some errors 01.06.23 08:38

--- src/context/auth/useAuthHook.tsx
+++ src/context/auth/useAuthHook.tsx
@@ -1,1 +1,30 @@
-export const useAuth = () => {};
+import { useContext, useEffect } from 'react';
+import AuthContext from './authContext';
+import { getUserWithCookie } from './authActions';
+
+export const useAuth = () => {
+  const { state: authState, dispatch } = useContext(AuthContext);
+
+  const { isLoading } = authState;
+
+  useEffect(() => {
+    (async () => {
+      dispatch({ type: 'SET_LOADING' });
+
+      const user = await getUserWithCookie();
+
+      if (user) {
+        dispatch({ type: 'SET_USER', payload: user });
+      } else {
+        dispatch({ type: 'RESET_USER' });
+      }
+
+      dispatch({ type: 'CLEAR_LOADING' });
+    })();
+  }, [dispatch]);
+
+  return {
+    authState,
+    isLoading,
+  };
+};
