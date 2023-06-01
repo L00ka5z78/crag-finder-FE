cf7fca6e2ae637f47358971085fa5cac446ae161
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-06-01 08:35:13 +0200

    custom logoutHook built. no errors 01.06.23 08:35

--- src/components/layout/UserHeader/useLogoutHook.tsx
+++ src/components/layout/UserHeader/useLogoutHook.tsx
@@ -1,6 +1,6 @@
 import { useNavigate } from 'react-router-dom';
 import { useContext } from 'react';
-
+import AuthContext from '../../../context/auth/authContext';
 import { logout } from '../../../services';
 import { userRoutes } from '../../../routes';
 
@@ -8,6 +8,12 @@ export const useLogoutHook = () => {
   const navigate = useNavigate();
   const { dispatch } = useContext(AuthContext);
 
-  const handleLogOut = async () => {};
+  const handleLogOut = async () => {
+    const response = await logout();
+    if (response?.status === 200) {
+      dispatch({ type: 'RESET_USER' });
+      navigate(userRoutes.home);
+    }
+  };
   return { handleLogOut };
 };
--- src/context/index.ts
+++ src/context/index.ts
@@ -2,4 +2,8 @@ export * from './search.context';
 export * from './messageModalContext';
 export * from './addFormModalContext';
 
+export * from './auth/authContext';
+export * from './auth/authReducer';
+export * from './auth/authActions';
+
 export * from './interfaces/modalsInterfaces';
