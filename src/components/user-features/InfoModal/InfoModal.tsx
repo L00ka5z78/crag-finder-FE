c5a59cddadd538e9ac623b72df716740356e7ce4
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-06-05 09:18:00 +0200

    extra feature (modal) in progress 05.06.23 09:27

---
+++ src/components/user-features/InfoModal/InfoModal.tsx
@@ -0,0 +1,10 @@
+type Props = {
+  modalVisible: boolean;
+  modalText: string;
+  closeModal: () => void;
+  linkPath?: string;
+};
+
+export const InfoModal: React.FC<Props> = () => {
+  return null;
+};
