f2cff0d2c3c28fa1a1a6d4386da1d09b6ce21487
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-06-05 09:01:39 +0200

    signUp basic featre in progress 05.06.23 09:01

--- src/components/user-features/SignUp/SignUp.tsx
+++ src/components/user-features/SignUp/SignUp.tsx
@@ -1,7 +1,26 @@
+import { useState } from "react";
+import { useIntl } from "react-intl";
+import LoadingSpinner from '../../../components/layout/Spinner/Spinner'
+
+import { messages } from "./messages";
+
 type SignUpForm = {
   email: string;
   password: string;
   password2: string;
 };
 
-export const SignUp = () => {};
+export const SignUp = () => {
+    const { formatMessage } = useIntl();
+
+      // Local state and hooks
+  const [loading, setLoading] = useState<boolean>(false);
+
+
+
+  if (loading) {
+    return <LoadingSpinner />;
+  }
+  return ()
+
+};
