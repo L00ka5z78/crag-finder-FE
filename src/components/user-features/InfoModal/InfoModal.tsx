8e56aee0a00b6312aab2db6d9503de3a98e1cf2a
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-06-05 09:51:38 +0200

    info modal added 05.06.23 09:51

--- src/components/user-features/InfoModal/InfoModal.tsx
+++ src/components/user-features/InfoModal/InfoModal.tsx
@@ -1,3 +1,5 @@
+import { Link } from 'react-router-dom';
+
 type Props = {
   modalVisible: boolean;
   modalText: string;
@@ -5,6 +7,26 @@ type Props = {
   linkPath?: string;
 };
 
-export const InfoModal: React.FC<Props> = () => {
-  return null;
+export const InfoModal: React.FC<Props> = ({
+  modalVisible,
+  modalText,
+  linkPath,
+  closeModal,
+}) => {
+  return (
+    <>
+      <div className="textModalContainer">
+        <div className="textModalBox">
+          <div className="textModalText">{modalText}</div>
+          {linkPath ? (
+            <Link to={linkPath} className="textModalLink">
+              <button className="textModalButton">OK</button>
+            </Link>
+          ) : (
+            <button className="textModalButton">OK</button>
+          )}
+        </div>
+      </div>{' '}
+    </>
+  );
 };
