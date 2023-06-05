d31bd4c0754016f7b201480d659cb0b0704004f9
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-06-05 13:55:41 +0200

    custom hook in progress 05.06.23 13:55

--- src/components/user-features/hooks/useForm/userFormHook.ts
+++ src/components/user-features/hooks/useForm/userFormHook.ts
@@ -1,3 +1,42 @@
-export const useFormHook = () => {
+import { useState } from 'react';
+import { Validate } from './validate';
+
+const setInitialErrors = <T extends Record<string, string>>(
+  initialFormState: T
+) => {
+  let initialErrors: Record<string, null> = {};
+
+  for (const key of Object.keys(initialFormState)) {
+    initialErrors[key] = null;
+  }
+
+  return initialErrors as Record<keyof T, null>;
+};
+
+export const useFormHook = <T extends Record<string, string>>(
+  initialFormState: T
+) => {
+  const [isError, setIsError] = useState(false);
+  const [errors, setErrors] = useState<Record<keyof T, string | null>>(
+    setInitialErrors(initialFormState)
+  );
+  const [form, setForm] = useState<T>(initialFormState);
+
+  const formValidators: Record<string, Validate> = {};
+  const errorMessages: Record<string, string> = {};
+
+  // It adds validators to the given form field and returns html tags
+  const tag = (name: string, validators: Validate, errorMessage?: string) => {
+    formValidators[name] = validators;
+    if (errorMessage) {
+      errorMessages[name] = errorMessage;
+    }
+
+    return {
+      name: name,
+      id: name,
+    };
+  };
+
   return null; // temporary
 };
