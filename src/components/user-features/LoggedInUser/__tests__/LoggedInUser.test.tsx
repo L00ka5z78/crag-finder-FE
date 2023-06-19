63e68df3f369dc9fe2ef749a1e77c8e2dbe880b2
Lukasz Bolach <bravo1_lima1@yahoo.com>
2023-06-19 09:21:18 +0200

    loggenInUser test added 19.96.23 09:21

---
+++ src/components/user-features/LoggedInUser/__tests__/LoggedInUser.test.tsx
@@ -0,0 +1,12 @@
+import { LoggedInUser } from '../LoggedInUser';
+import { render, screen } from '@testing-library/react';
+
+describe('LoggedInUser', () => {
+  it('should render LoggedInUser', () => {
+    render(<LoggedInUser />);
+    const component = screen.getByText('Todo LogOut');
+    console.log(component);
+    expect(component).toBeInTheDocument();
+    screen.getByRole('heading');
+  });
+});
